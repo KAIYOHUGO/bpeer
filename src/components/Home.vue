008f76c219e4652053ab02d98a443dbb0576d010
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2021-09-18 11:31:12 +0000

    add home page✨

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1>home</h1>
+    <div class="w-full h-full flex flex-col justify-center items-center">
+        <h1 class="text-5xl p-10">BPeer</h1>
+        <div class="flex flex-col">
+            <input
+                type="text"
+                class="rounded self-center p-2 m-2 text-lg"
+                placeholder="ROOM ID"
+                v-model="id"
+            />
+            <rounded-button @click="$router.push(`/r/${id}`)" :disabled="!id">
+                Join Peer
+            </rounded-button>
+            <h1 class="text-2xl text-center">OR</h1>
+            <rounded-button @click="$router.push(`/h/${id}`)" :disabled="!id">
+                Host Peer
+            </rounded-button>
+        </div>
+    </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import RoundedButton from "@/components/RoundedButton.vue";
+
+export default defineComponent({
+    data() {
+        return {
+            id: "",
+        };
+    },
+    components: {
+        RoundedButton,
+    },
+});
+</script>
